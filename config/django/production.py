94e19b0882ba3ce8e28b151913f9248dab8d9141
Jobe Moraes <jobecamera@gmail.com>
2025-03-16 17:03:05 -0300

    core: installed apps prod whitenoise nostatic

--- config/django/production.py
+++ config/django/production.py
@@ -1,6 +1,9 @@
 from .base import *
 from config.env import env
 
+INSTALLED_APPS = ['whitenoise.runserver_nostatic'] + INSTALLED_APPS
+
+
 DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
