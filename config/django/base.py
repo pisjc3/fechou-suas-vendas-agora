54c3b5329006d1707169c1f261f5c53d3bb74a19
Jobe Moraes <jobecamera@gmail.com>
2025-03-16 15:47:47 -0300

    core: env

--- config/django/base.py
+++ config/django/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(",")
 
 
 # Application definition
