1ef14b5634eb4fdc8640cd11afa15896b5a92fb8
Jobe Moraes <jobecamera@gmail.com>
2025-03-23 11:09:52 -0300

    refactor: mostrar id no admin Empresa e UsuarioEmpresa

--- crm_apps/crm/admin.py
+++ crm_apps/crm/admin.py
@@ -6,13 +6,13 @@ from django.contrib import admin
 
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cnpj', 'endereco', 'telefone', 'criado_por')
+    list_display = ('id', 'nome', 'cnpj', 'endereco', 'telefone', 'criado_por')
     search_fields = ('nome', 'cnpj', 'endereco', 'telefone', 'criado_por')
     ordering = ('-id',)
 
 
 @admin.register(UsuarioEmpresa)
 class UsuarioEmpresaAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'empresa')
+    list_display = ('id', 'usuario', 'empresa')
     search_fields = ('usuario', 'empresa')
     ordering = ('-id',)
