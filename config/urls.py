11bcb43a58fa3f5654a1efea87444c7fd1c3c995
Jobe Moraes <jobecamera@gmail.com>
2025-04-05 09:45:47 -0300

    core: upload de imagens em producao

--- config/urls.py
+++ config/urls.py
@@ -14,14 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('crm_apps.users.urls')),
     path('', include('crm_apps.crm.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Produção
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
+]
+
+# Desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
