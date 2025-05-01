0ca1d86791dadc63482da9715464c8ae653e97ec
Jobe Moraes <jobecamera@gmail.com>
2025-05-01 15:50:49 -0300

    refactor: sugestao da comunidade externa: troca do nome Compra para Entrada

--- crm_apps/crm/movimentacao/urls.py
+++ crm_apps/crm/movimentacao/urls.py
@@ -4,7 +4,7 @@ from .views import VendaCreateView, CompraCreateView, MovimentacaoListView, Vend
 urlpatterns = [
     path('', MovimentacaoListView.as_view(), name='movimentacao_list'),
     path('venda/', VendaListView.as_view(), name='venda_list'),
-    path('compra/', CompraListView.as_view(), name='compra_list'),
+    path('entrada/', CompraListView.as_view(), name='compra_list'),
     path('nova-venda/', VendaCreateView.as_view(), name='venda_create'),
-    path('nova-compra/', CompraCreateView.as_view(), name='compra_create'),
+    path('nova-entrada/', CompraCreateView.as_view(), name='compra_create'),
 ]
