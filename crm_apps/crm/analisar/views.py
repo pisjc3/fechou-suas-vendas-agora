a07d2503a23f775fef4a049a3140f526eea95011
Jobe Moraes <jobecamera@gmail.com>
2025-05-02 16:40:56 -0300

    fix: data e dados por empresa no analisar

--- crm_apps/crm/analisar/views.py
+++ crm_apps/crm/analisar/views.py
@@ -2,12 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.http import JsonResponse
 from django.views.generic import TemplateView
-from datetime import datetime, timedelta
 from crm_apps.crm.cliente.models import Cliente
 from crm_apps.crm.movimentacao.models import Movimentacao
 from crm_apps.crm.util.selectors import get_empresa_do_usuario
 from django.contrib import messages
 from datetime import datetime, timedelta
+from django.utils import timezone
+from django.utils.timezone import now, make_aware
 
 
 @method_decorator(login_required, name='dispatch')
@@ -28,45 +29,67 @@ class AnalisarListView(TemplateView):
             return
 
         # Dados para os gráficos iniciais (últimos 30 dias)
-        data_fim = datetime.now()
+        data_fim = now()
         data_inicio = data_fim - timedelta(days=30)
 
+        data_inicio = timezone.make_aware(
+            datetime.combine(data_inicio, datetime.min.time()), timezone.get_current_timezone())
+        data_fim = timezone.make_aware(
+            datetime.combine(data_fim, datetime.max.time()), timezone.get_current_timezone())
+
         context['clientes_data'] = self.get_clientes_data(
-            data_inicio, data_fim)
+            data_inicio, data_fim, usuario, empresa)
         context['movimentacoes_data'] = self.get_movimentacoes_data(
-            data_inicio, data_fim)
+            data_inicio, data_fim, usuario, empresa)
         context['produtos_data'] = self.get_produtos_data(
-            data_inicio, data_fim)
+            data_inicio, data_fim, usuario, empresa)
         context['vendas_por_cliente_data'] = self.get_vendas_por_cliente(
-            data_inicio, data_fim)
+            data_inicio, data_fim, usuario, empresa)
 
         return context
 
-    def get_clientes_data(self, data_inicio, data_fim):
-        data_fim = datetime.combine(data_fim, datetime.max.time())
+    def get_clientes_data(self, data_inicio, data_fim, usuario, empresa):
+        clientes = None
 
-        clientes = Cliente.objects.filter(
-            data_criacao__gt=data_inicio,
-            data_criacao__lt=data_fim
-        )
+        if usuario.is_superuser:
+            clientes = Cliente.objects.filter(
+                data_criacao__gte=data_inicio,
+                data_criacao__lte=data_fim,
+            )
+        else:
+            clientes = Cliente.objects.filter(
+                data_criacao__gte=data_inicio,
+                data_criacao__lte=data_fim,
+                empresa=empresa
+            )
 
         clientes_por_dia = {}
         for cliente in clientes:
             dia = cliente.data_criacao.strftime('%d/%m/%Y')
             clientes_por_dia[dia] = clientes_por_dia.get(dia, 0) + 1
 
+        clientes_por_dia = dict(sorted(clientes_por_dia.items(
+        ), key=lambda item: datetime.strptime(item[0], '%d/%m/%Y')))
+
         return {
             'labels': list(clientes_por_dia.keys()),
             'values': list(clientes_por_dia.values())
         }
 
-    def get_movimentacoes_data(self, data_inicio, data_fim):
-        data_fim = datetime.combine(data_fim, datetime.max.time())
+    def get_movimentacoes_data(self, data_inicio, data_fim, usuario, empresa):
+        movimentacoes = None
 
-        movimentacoes = Movimentacao.objects.filter(
-            data_criacao__gte=data_inicio,
-            data_criacao__lte=data_fim
-        )
+        if usuario.is_superuser:
+            movimentacoes = Movimentacao.objects.filter(
+                data_criacao__gte=data_inicio,
+                data_criacao__lte=data_fim,
+            )
+        else:
+            movimentacoes = Movimentacao.objects.filter(
+                data_criacao__gte=data_inicio,
+                data_criacao__lte=data_fim,
+                empresa=empresa
+            )
 
         vendas_por_dia = {}
         compras_por_dia = {}
@@ -78,34 +101,42 @@ class AnalisarListView(TemplateView):
             elif mov.tipo == 'compra':
                 compras_por_dia[dia] = compras_por_dia.get(dia, 0) + 1
 
-        # Para garantir que todos os dias existam, mesmo que zerados
         todos_os_dias = set(list(vendas_por_dia.keys()) +
                             list(compras_por_dia.keys()))
         todos_os_dias = sorted(
-            todos_os_dias, key=lambda d: datetime.strptime(d, '%d/%m/%Y'))
+            todos_os_dias, key=lambda d: datetime.strptime(d, '%d/%m/%Y')
+        )
 
         vendas = [vendas_por_dia.get(dia, 0) for dia in todos_os_dias]
         compras = [compras_por_dia.get(dia, 0) for dia in todos_os_dias]
 
-        return {
+        movimentacoes_por_dia = {
             'labels': todos_os_dias,
             'vendas': vendas,
             'compras': compras
         }
 
-    def get_produtos_data(self, data_inicio, data_fim):
-        data_fim = datetime.combine(data_fim, datetime.max.time())
+        return movimentacoes_por_dia
 
-        movimentacoes = Movimentacao.objects.filter(
-            data_criacao__gte=data_inicio,
-            data_criacao__lte=data_fim
-        )
+    def get_produtos_data(self, data_inicio, data_fim, usuario, empresa):
+        movimentacoes = None
+
+        if usuario.is_superuser:
+            movimentacoes = Movimentacao.objects.filter(
+                data_criacao__gte=data_inicio,
+                data_criacao__lte=data_fim,
+            )
+        else:
+            movimentacoes = Movimentacao.objects.filter(
+                data_criacao__gte=data_inicio,
+                data_criacao__lte=data_fim,
+                empresa=empresa
+            )
 
         produtos_vendidos = {}
         produtos_comprados = {}
 
         for mov in movimentacoes:
-            dia = mov.data_criacao.strftime('%d/%m/%Y')
             produto_nome = mov.produto.nome
             quantidade = mov.quantidade
 
@@ -134,11 +165,20 @@ class AnalisarListView(TemplateView):
             'compras': compras
         }
 
-    def get_vendas_por_cliente(self, data_inicio, data_fim):
-        data_fim = datetime.combine(data_fim, datetime.max.time())
+    def get_vendas_por_cliente(self, data_inicio, data_fim, usuario, empresa):
+        movimentacoes = None
 
-        movimentacoes = Movimentacao.objects.filter(
-            data_criacao__gte=data_inicio, data_criacao__lte=data_fim)
+        if usuario.is_superuser:
+            movimentacoes = Movimentacao.objects.filter(
+                data_criacao__gte=data_inicio,
+                data_criacao__lte=data_fim,
+            )
+        else:
+            movimentacoes = Movimentacao.objects.filter(
+                data_criacao__gte=data_inicio,
+                data_criacao__lte=data_fim,
+                empresa=empresa
+            )
 
         vendas_por_cliente = {}
 
@@ -157,34 +197,46 @@ class AnalisarListView(TemplateView):
         }
 
     def get(self, request, *args, **kwargs):
-        # Verificar se é uma requisição AJAX verificando o cabeçalho HTTP_X_REQUESTED_WITH
         if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             tipo = request.GET.get('tipo')
             data_inicio = request.GET.get('data_inicio')
             data_fim = request.GET.get('data_fim')
 
+            usuario = self.request.user
+            is_superuser = usuario.is_superuser
+            empresa = None
+
+            empresa = get_empresa_do_usuario(usuario_id=usuario.id)
+            if not empresa and not is_superuser:
+                messages.error(
+                    self.request, "Você não tem uma empresa associada.")
+                return
+
             if not data_inicio or not data_fim:
                 return JsonResponse({'error': 'Datas de início e fim são obrigatórias.'}, status=400)
 
             try:
-                data_inicio_dt = datetime.strptime(data_inicio, '%Y-%m-%d')
-                data_fim_dt = datetime.strptime(data_fim, '%Y-%m-%d')
+                data_inicio_dt = make_aware(
+                    datetime.strptime(data_inicio, '%Y-%m-%d'))
+                data_fim_dt = make_aware(
+                    datetime.strptime(data_fim, '%Y-%m-%d'))
             except ValueError:
                 return JsonResponse({'error': 'Formato de data inválido.'}, status=400)
 
             if tipo == 'cliente':
-                dados = self.get_clientes_data(data_inicio_dt, data_fim_dt)
+                dados = self.get_clientes_data(
+                    data_inicio_dt, data_fim_dt, usuario, empresa)
             elif tipo == 'movimentacao':
                 dados = self.get_movimentacoes_data(
-                    data_inicio_dt, data_fim_dt)
+                    data_inicio_dt, data_fim_dt, usuario, empresa)
             elif tipo == 'produto':
-                dados = self.get_produtos_data(data_inicio_dt, data_fim_dt)
+                dados = self.get_produtos_data(
+                    data_inicio_dt, data_fim_dt, usuario, empresa)
             elif tipo == 'vendas_por_cliente':
                 dados = self.get_vendas_por_cliente(
-                    data_inicio_dt, data_fim_dt)
+                    data_inicio_dt, data_fim_dt, usuario, empresa)
             else:
                 return JsonResponse({'error': 'Tipo inválido.'}, status=400)
 
             return JsonResponse(dados)
-
         return super().get(request, *args, **kwargs)
--- static/global/js/analisar.js
+++ static/global/js/analisar.js
@@ -23,26 +23,25 @@ async function filtrarTodosGraficos() {
         alert("Erro: " + dados.error);
         continue;
       }
-
-      atualizarGrafico(tipo, dados);
+      if (tipo === "cliente") {
+        criarGraficoClientes(dados);
+      }
+      if (tipo === "movimentacao") {
+        criarGraficoMovimentacoes(dados);
+      }
+      if (tipo === "produto") {
+        criarGraficoProdutos(dados);
+      }
+      if (tipo === "vendas_por_cliente") {
+        criarGraficoVendasPorCliente(dados);
+      }
     } catch (error) {
       console.error("Erro ao filtrar gráfico:", tipo, error);
     }
   }
 }
 
-function atualizarGrafico(tipo, dados) {
-  if (charts[tipo]) {
-    charts[tipo].data.labels = dados.labels;
-    charts[tipo].data.datasets[0].data = dados.values;
-    charts[tipo].update();
-  } else {
-    console.error("Gráfico não encontrado para o tipo:", tipo);
-  }
-}
-
-function criarGraficoLine(canvasId, labels, datasets) {
-  const ctx = document.getElementById(canvasId).getContext("2d");
+function criarGraficoLine(ctx, labels, datasets, titleX, titleY) {
   return new Chart(ctx, {
     type: "line",
     data: {
@@ -54,84 +53,271 @@ function criarGraficoLine(canvasId, labels, datasets) {
       tension: 0.1,
       fill: false,
       scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: titleX,
+          },
+        },
         x: {
+          title: {
+            display: true,
+            text: titleY,
+          },
           ticks: {
             minRotation: 45,
             maxRotation: 45,
             callback: function (value, index, values) {
-              const maxLength = 10; 
-              let labelValue = value; 
-
-              if (Array.isArray(labels) && labels[index]) {
-                labelValue = labels[index];
-              }
-
-              if (labelValue.length > maxLength) {
-                return labelValue.substring(0, maxLength) + "...";
-              }
-              return labelValue;
+              return textTransformEllipsis(
+                value,
+                index,
+                this.chart.data.labels
+              );
             },
           },
+        },
+      },
+      plugins: {
+        legend: {
+          display: true,
+          position: "top",
+        },
+        tooltip: {
+          mode: "index",
+          intersect: false,
         },
       },
     },
   });
 }
 
-function criarGraficoBar(canvasId, labels, datasets) {
-  const ctx = document.getElementById(canvasId).getContext("2d");
+function criarGraficoBar(ctx, labels, datasets, titleX, titleY) {
   return new Chart(ctx, {
     type: "bar",
     data: {
-      labels: labels, 
+      labels: labels,
       datasets: datasets,
     },
     options: {
       responsive: true,
       scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            stepSize: 1,
+            precision: 0,
+          },
+          title: {
+            display: true,
+            text: titleX,
+          },
+        },
         x: {
+          title: {
+            display: true,
+            text: titleY,
+          },
           ticks: {
             minRotation: 45,
             maxRotation: 45,
             callback: function (value, index, values) {
-              const maxLength = 10; 
-              let labelValue = value; 
-
-              if (Array.isArray(labels) && labels[index]) {
-                labelValue = labels[index];
-              }
-
-              if (labelValue.length > maxLength) {
-                return labelValue.substring(0, maxLength) + "...";
-              }
-              return labelValue;
+              return textTransformEllipsis(
+                value,
+                index,
+                this.chart.data.labels
+              );
             },
           },
+        },
+      },
+      plugins: {
+        legend: {
+          display: true,
+          position: "top",
+        },
+        tooltip: {
+          mode: "index",
+          intersect: false,
         },
       },
     },
   });
 }
 
-function selecionarAnalisarGrafico(){
-  const graficoSelect = document.getElementById('graficoSelect');
+function selecionarAnalisarGrafico() {
+  const graficoSelect = document.getElementById("graficoSelect");
 
-  graficoSelect.addEventListener('change', function () {
+  graficoSelect.addEventListener("change", function () {
     const selected = graficoSelect.value;
-    const sections = document.querySelectorAll('.grafico');
+    const sections = document.querySelectorAll(".grafico");
 
-    if (selected === 'todos') {
-      sections.forEach(section => {
-        section.style.display = 'block';
+    if (selected === "todos") {
+      sections.forEach((section) => {
+        section.style.display = "block";
       });
     } else {
-      sections.forEach(section => {
+      sections.forEach((section) => {
         if (section.id === `grafico_${selected}`) {
-          section.style.display = 'block';
+          section.style.display = "block";
         } else {
-          section.style.display = 'none';
+          section.style.display = "none";
         }
       });
     }
   });
+}
+
+function criarGraficoClientes(clientesData) {
+  const ctx = document.getElementById("clientesChart").getContext("2d");
+
+  if (charts.clientes) {
+    charts.clientes.destroy();
+  }
+
+  const labels = clientesData.labels;
+
+  const datasets = [
+    {
+      label: "Clientes",
+      data: clientesData.values,
+      backgroundColor: "rgba(54, 162, 235, 0.6)",
+      borderColor: "rgba(54, 162, 235, 1)",
+      borderWidth: 1,
+    },
+  ];
+
+  const titleX = "Quantidade de Clientes";
+  const titleY = "Período";
+
+  charts.clientes = criarGraficoBar(ctx, labels, datasets, titleX, titleY);
+}
+
+function criarGraficoMovimentacoes(movimentacoesData) {
+  const ctx = document.getElementById("movimentacoesChart").getContext("2d");
+
+  if (charts.movimentacoes) {
+    charts.movimentacoes.destroy();
+  }
+
+  const labels = movimentacoesData.labels;
+
+  const datasets = [
+    {
+      label: "Vendas",
+      data: movimentacoesData.vendas,
+      borderColor: "#198754",
+      backgroundColor: "rgba(25, 135, 84, 0.2)",
+      tension: 0.1,
+      fill: false,
+    },
+    {
+      label: "Entradas",
+      data: movimentacoesData.compras,
+      borderColor: "#ffc107",
+      backgroundColor: "rgba(255, 193, 7, 0.2)",
+      tension: 0.1,
+      fill: false,
+    },
+  ];
+
+  const titleX = "Quantidade de movimentações";
+  const titleY = "Período";
+
+  charts.movimentacoes = criarGraficoBar(ctx, labels, datasets, titleX, titleY);
+}
+
+function criarGraficoProdutos(produtosData) {
+  const ctx = document.getElementById("produtosChart").getContext("2d");
+
+  if (charts.produtos) {
+    charts.produtos.destroy();
+  }
+
+  const labels = produtosData.labels;
+
+  const datasets = [
+    {
+      label: "Vendas",
+      data: produtosData.vendas,
+      borderColor: "#198754",
+      backgroundColor: "rgba(25, 135, 84, 0.2)",
+      tension: 0.1,
+      fill: false,
+    },
+    {
+      label: "Entradas",
+      data: produtosData.compras,
+      borderColor: "#ffc107",
+      backgroundColor: "rgba(255, 193, 7, 0.2)",
+      tension: 0.1,
+      fill: false,
+    },
+  ];
+
+  const titleX = "Quantidade de movimentações";
+  const titleY = "Produto";
+
+  charts.produtos = criarGraficoLine(ctx, labels, datasets, titleX, titleY);
+}
+
+function criarGraficoVendasPorCliente(vendasPorClienteData) {
+  const ctx = document.getElementById("vendasPorClienteChart").getContext("2d");
+
+  if (charts.vendas_por_cliente) {
+    charts.vendas_por_cliente.destroy();
+  }
+
+  const labels = vendasPorClienteData.labels;
+
+  const datasets = [
+    {
+      label: "Vendas por cliente",
+      data: vendasPorClienteData.values,
+      backgroundColor: "rgba(255, 159, 64, 0.6)",
+      borderColor: "rgba(255, 159, 64, 1)",
+      borderWidth: 1,
+    },
+  ];
+
+  const titleX = "Quantidade de vendas";
+  const titleY = "Cliente";
+
+  charts.vendas_por_cliente = criarGraficoBar(
+    ctx,
+    labels,
+    datasets,
+    titleX,
+    titleY
+  );
+}
+
+function textTransformEllipsis(value, index, labels) {
+  const maxLength = 10;
+  let labelValue = value;
+
+  if (Array.isArray(labels) && labels[index]) {
+    labelValue = labels[index];
+  }
+
+  if (labelValue.length > maxLength) {
+    return labelValue.substring(0, maxLength) + "...";
+  }
+  return labelValue;
+}
+
+function isGraficoEmpty(obj) {
+  for (const key in obj) {
+    if (Array.isArray(obj[key]) && obj[key].length > 0) return false;
+    if (obj[key] && !Array.isArray(obj[key])) return false;
+  }
+  return true;
+}
+
+function mensagemGraficoSemDados(elementId) {
+  const el = document.getElementById(elementId);
+  if (el) {
+    el.innerHTML = 'Sem dados.';
+    el.style.display = 'block';
+  }
 }
